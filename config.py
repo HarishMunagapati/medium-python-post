1f31476c4bbd0848cc5c033b672144b08656287c
Harish Munagapati <hmunagapati@outlook.com>
2021-04-19 09:05:42 +0530

    update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 # Configuration file
 
+# To get user_id add api_token below and run "getuserinfo.py"
+# that will output user information, grab "id" and add it here.
 user_id = "Your-User-ID"
 api_token = "Your-API-token"
